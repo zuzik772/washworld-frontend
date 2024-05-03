bea5738f1661429cae7b02e558bf57532c4a6adb
emkataumre <emo.vladinov@gmail.com>
2024-05-03 14:50:03 +0200

    fixed excessive spacing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,23 +3,23 @@ import React from "react";
 import { Text, View } from "react-native";
 
 type LayoutProps = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-	return (
-		<View className="flex-1 items-center justify-center bg-primaryGreen">
-			<Text>Testing</Text>
-			<Box>
-				<Pressable
-					onPress={() => console.log("Clicked")}
-					backgroundColor="$primaryGreen"
-				>
-					<Text className="text-white">Press me</Text>
-				</Pressable>
-			</Box>
-			{children}
-		</View>
-	);
+  return (
+    <View className="flex-1 items-center justify-center bg-primaryGreen">
+      <Text>Testing</Text>
+      <Box>
+        <Pressable
+          onPress={() => console.log("Clicked")}
+          backgroundColor="$primaryGreen"
+        >
+          <Text className="text-white">Press me</Text>
+        </Pressable>
+      </Box>
+      {children}
+    </View>
+  );
 };
 export default Layout;
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -4,21 +4,21 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import Layout from "../components/Layout";
 const MapScreen = () => {
-	return (
-		<Layout>
-			<Text>MapScreen</Text>
-			<Box
-				backgroundColor="$primaryGreen"
-				padding={10}
-				justifyContent="center"
-				alignItems="center"
-			>
-				<Text color="$secondaryOrange" fontWeight="$bold">
-					BOX
-				</Text>
-			</Box>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Text>MapScreen</Text>
+      <Box
+        backgroundColor="$primaryGreen"
+        padding={10}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text color="$secondaryOrange" fontWeight="$bold">
+          BOX
+        </Text>
+      </Box>
+    </Layout>
+  );
 };
 export default MapScreen;
 const styles = StyleSheet.create({});
