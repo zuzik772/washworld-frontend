f36fbaff4a59045861f5648b021712987bd1fe3f
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-05-06 11:54:46 +0200

    map satck navigation works

--- screens/map/LocationScreen.tsx
+++ screens/map/LocationScreen.tsx
@@ -1,11 +1,19 @@
 import { Text } from "react-native";
 import React from "react";
 import Layout from "../../components/Layout";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { MapStackParamList } from "../../navigation/MapStackParamList";
+import NavButton from "../../components/NavButton";
 
-const LocationScreen = () => {
+type Props = NativeStackScreenProps<MapStackParamList, "Location">;
+const LocationScreen = ({ navigation }: Props) => {
   return (
     <Layout>
       <Text className="text-white">LocationScreen</Text>
+      <NavButton
+        title="Select Wash"
+        onPress={() => navigation.navigate("Package")}
+      />
     </Layout>
   );
 };
--- screens/map/PreWashScreen.tsx
+++ screens/map/PreWashScreen.tsx
@@ -1,11 +1,16 @@
 import { Text } from "react-native";
 import React from "react";
 import Layout from "../../components/Layout";
+import { MapStackParamList } from "../../navigation/MapStackParamList";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import NavButton from "../../components/NavButton";
 
-const PreWashScreen = () => {
+type Props = NativeStackScreenProps<MapStackParamList, "PreWash">;
+const PreWashScreen = ({ navigation }: Props) => {
   return (
     <Layout>
-      <Text className="text-white">PreWashScreen</Text>
+      <Text className="text-primaryWhite">Prewashscreen</Text>
+      <NavButton title="Start" onPress={() => navigation.navigate("Wash")} />
     </Layout>
   );
 };
