66d64058d52e29116c1f10fcd649c708e4612ba3
emkataumre <emo.vladinov@gmail.com>
2024-05-12 14:34:15 +0200

    added and animated  the traker bars

--- screens/map/PackageScreen.tsx
+++ screens/map/PackageScreen.tsx
@@ -1,29 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout";
-import { Heading, View } from "@gluestack-ui/themed";
+import { Box, Heading, View } from "@gluestack-ui/themed";
 import PackageCard from "../../components/PackageCard";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import Carousel from "react-native-reanimated-carousel";
-import { Dimensions } from "react-native";
+import {
+  Dimensions,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+} from "react-native";
 
 const PackageScreen = () => {
   const packages = useSelector((state: RootState) => state.packages.packages);
   const width = Dimensions.get("window").width;
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <Layout>
-      <View className="flex items-center mt-6">
+      <View className="flex items-center mt-3">
         <Heading fontSize={25} className="text-primaryWhite">
           Select Wash
         </Heading>
       </View>
+      <View className="flex flex-row gap-2 items-center justify-center mb-7">
+        <Box
+          className={
+            currentIndex === 0
+              ? "w-10 h-1 bg-primaryGreen transition-all"
+              : "w-7 h-1 bg-primaryGreen opacity-50 transition-all"
+          }
+        ></Box>
+        <Box
+          className={
+            currentIndex === 1
+              ? "w-10 h-1 bg-primaryGreen transition-all"
+              : "w-7 h-1 bg-primaryGreen opacity-50 transition-all"
+          }
+        ></Box>
+        <Box
+          className={
+            currentIndex === 2
+              ? "w-10 h-1 bg-primaryGreen transition-all"
+              : "w-7 h-1 bg-primaryGreen opacity-50 transition-all"
+          }
+        ></Box>
+      </View>
       <Carousel
+        loop={false}
         width={width / 2}
         height={width * 2}
         style={{ width: width }}
         snapEnabled
         data={packages}
+        onScrollEnd={setCurrentIndex}
         scrollAnimationDuration={500}
         renderItem={({ item, index }) => (
           <PackageCard key={index} subscription={item} /> //using subscription instead of "package", because "package" is a reserved keyword
