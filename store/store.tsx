5aa23f37bff111c72cb557a5c4958866a80def50
Tsukani <34612305+Tsukani@users.noreply.github.com>
2024-05-20 15:09:32 +0200

    Update store.tsx

--- store/store.tsx
+++ store/store.tsx
@@ -6,6 +6,7 @@ import thunk from "redux-thunk";
 export const store = configureStore({
   reducer: {},
 });
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
