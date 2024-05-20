e0ceb8a0e456d4851dc46cb957fd4bf4ce4e296c
emkataumre <emo.vladinov@gmail.com>
2024-05-20 15:07:15 +0200

    refactor: Update location types and add new properties to locationSlice

--- types/location.ts
+++ types/location.ts
@@ -1,3 +1,15 @@
+export type location = {
+  location_id: number;
+  address: string;
+  latitude: number;
+  longitude: number;
+  opening_times: string;
+  closing_times: string;
+  hall_id: number;
+  status: Status;
+  isFavourite: boolean;
+};
+
 export interface SelfWash {
   status_id: number;
   status: Status;
